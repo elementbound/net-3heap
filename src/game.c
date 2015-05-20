283d9dde6760765e2f3cdbd611d7125dc4646f8a
Gálffy Tamás <ezittgtx@gmail.com>
2015-05-20 12:30:18 +0200

    game_State now supports variable heap count but defaults to 3 Heap count can only be changed by sync

--- src/game.c
+++ src/game.c
@@ -4,26 +4,28 @@
 #define HEAP_COUNT 3
 
 typedef struct {
-	int heap[HEAP_COUNT];
-	int maxItemsPerTurn;
+	int*	heap;
+	int		heapCount;
+	int		maxItemsPerTurn;
 } game_State;
 
 game_State* game_Init(int itemsPerHeap, int maxItemsPerTurn) {
 	game_State* game = (game_State*)malloc(sizeof(game_State));
 
-	for(int i = 0; i < HEAP_COUNT; i++)
-		game->heap[i] = 0;
+	game->heapCount = HEAP_COUNT;
+	game->heap = (int*)calloc(game->heapCount, sizeof(int));
 	game->maxItemsPerTurn = maxItemsPerTurn;
 
 	return game;
 }
 
 void game_Free(game_State* game) {
+	free(game->heap);
 	free(game);
 }
 
 bool game_Turn(game_State* game, int heapId, int itemCount) {
-	if(heapId >= HEAP_COUNT) 
+	if(heapId >= game->heapCount) 
 		return false;
 
 	if(itemCount > game->maxItemsPerTurn)
@@ -37,13 +39,13 @@ bool game_Turn(game_State* game, int heapId, int itemCount) {
 }
 
 bool game_IsFinished(game_State* game) {
-	for(int i = 0; i < HEAP_COUNT; i++) 
+	for(int i = 0; i < game->heapCount; i++) 
 		if(game->heap[i] != 0)
 			return false;
 
 	return true;
 }
 
-int game_GetHeapCount(game_State* game) { return HEAP_COUNT; }
-int game_GetHeap(game_State* game, int heapId) { return (heapId < HEAP_COUNT) ? game->heap[heapId] : -1; }
+int game_GetHeapCount(game_State* game) { return game->heapCount; }
+int game_GetHeap(game_State* game, int heapId) { return (heapId < game->heapCount) ? game->heap[heapId] : -1; }
 int game_GetMaxItemsPerTurn(game_State* game) { return game->maxItemsPerTurn; }
